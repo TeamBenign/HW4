e29d029cb2d9976f1da650ed6e222527aaa87354
Kishan Kumar Ganguly <kkganguly.iit.du@gmail.com>
2024-10-21 20:50:50 +0000

    single line pipe

--- task3.sh
+++ task3.sh
@@ -1,4 +1,1 @@
-sed -i 's/\r$//' titanic.csv & \
-gawk -v FPAT="[^,\"]*|\"([^\"]|\"\")*\"" '$3==2 && $12=="S" { print  $0}' titanic.csv | \
-sed 's/female/F/g; s/male/M/g' | \
-gawk -v FPAT="[^,\"]*|\"([^\"]|\"\")*\"" '$6 != "" {sum += $6;count++} END {print sum/count}'
+sed -i 's/\r$//' titanic.csv & gawk -v FPAT="[^,\"]*|\"([^\"]|\"\")*\"" '$3==2 && $12=="S" { print  $0}' titanic.csv | sed 's/female/F/g; s/male/M/g' | gawk -v FPAT="[^,\"]*|\"([^\"]|\"\")*\"" '$6 != "" {sum += $6;count++} END {print sum/count}'
