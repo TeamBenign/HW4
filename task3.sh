b1301ec3713d3471d9642bc29c96197bc7cc9dac
Kishan Kumar Ganguly <kkganguly.iit.du@gmail.com>
2024-10-21 20:47:47 +0000

    updated task3.sh

--- task3.sh
+++ task3.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Convert the line endings from Windows (CRLF) to Unix (LF). Then proceed
-filtered_data_avg=$(sed -i 's/\r$//' titanic.csv & \
+sed -i 's/\r$//' titanic.csv & \
 gawk -v FPAT="[^,\"]*|\"([^\"]|\"\")*\"" '$3==2 && $12=="S" { print  $0}' titanic.csv | \
 sed 's/female/F/g; s/male/M/g' | \
-gawk -v FPAT="[^,\"]*|\"([^\"]|\"\")*\"" '$6 != "" {sum += $6;count++} END {print sum/count}')
-echo $filtered_data_avg
+gawk -v FPAT="[^,\"]*|\"([^\"]|\"\")*\"" '$6 != "" {sum += $6;count++} END {print sum/count}'
