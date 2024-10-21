b215e47e3303d0a21e018e255ee390972a1945c1
Kishan Kumar Ganguly <kkganguly.iit.du@gmail.com>
2024-10-21 20:49:53 +0000

    updated task3.sh

--- task3.sh
+++ task3.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-# Convert the line endings from Windows (CRLF) to Unix (LF). Then proceed
 sed -i 's/\r$//' titanic.csv & \
 gawk -v FPAT="[^,\"]*|\"([^\"]|\"\")*\"" '$3==2 && $12=="S" { print  $0}' titanic.csv | \
 sed 's/female/F/g; s/male/M/g' | \
