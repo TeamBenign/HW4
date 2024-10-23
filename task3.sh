4c28a9dfa4fcace213acbc6396ffad142e183f7a
Kishan Kumar Ganguly <kkganguly.iit.du@gmail.com>
2024-10-23 02:56:57 +0000

    updated task3.sh

--- task3.sh
+++ task3.sh
@@ -1,1 +1,1 @@
-sed -i 's/\r$//' titanic.csv & gawk -v FPAT="[^,\"]*|\"([^\"]|\"\")*\"" '$3==2 && $12=="S" { print  $0}' titanic.csv | sed 's/female/F/g; s/male/M/g' | gawk -v FPAT="[^,\"]*|\"([^\"]|\"\")*\"" '$6 != "" {sum += $6;count++} END {print sum/count}'
+sed -i 's/\r$//' titanic.csv & gawk -v FPAT="[^,\"]*|\"([^\"]|\"\")*\"" '$3==2 && $12=="S" { print  $0}' titanic.csv | sed 's/female/F/g; s/male/M/g' | gawk -v FPAT="[^,\"]*|\"([^\"]|\"\")*\"" '{print $0} $6 != "" {sum += $6;count++} END {print "The result is " sum/count}'
